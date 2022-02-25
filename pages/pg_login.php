9ec796c9c423a53a3f48c15d87163c881bbb2d23
Davi ferreira de sena <davifsena2@gmail.com>
2022-02-25 10:30:22 -0300

    pg login em andamento

--- pages/pg_login.php
+++ pages/pg_login.php
@@ -1,5 +1,8 @@
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,7 +13,38 @@
 
     <title>PetCare</title>
 </head>
-<body>
-    
+
+<body id="pg_cadastro">
+
+    <section id="cadastro-usu">
+
+        <form id="form_cadastro_usu" action="../action/insert_usuario.php" method="post">
+
+            <span>Fazer loginaaa</span>
+
+                <div class="container-input-dados">
+                    <input class="input-dados" type="number" required name="cpf" id="cpf">
+                    <label class="label-animado" for="cpf">Cpf</label>
+                </div>
+
+                <div class="container-input-dados">
+                    <input class="input-dados" type="password" required name="senha" id="senha">
+                    <label class="label-animado" for="tel">Senha</label>
+                </div>
+
+            <div class="container-button-dados">
+                <input id="button-dados" type="submit" value="Fazer cadastro">
+                <a href="pg_login.php">Já tenho uma conta</a>
+            </div>
+
+        </form>
+    </section>
+
+    <section id="pg-cadastro-fundo">
+        <figure>
+            <img src="../img/fundo.png" alt="">
+        </figure>
+    </section>
 </body>
+
 </html>
