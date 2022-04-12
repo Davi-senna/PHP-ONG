6441ae7881a36942c9ccfa661c3b6b16be2ded20
Davi ferreira de sena <davifsena2@gmail.com>
2022-04-12 09:59:26 -0300

    Implementando contoller image, criar array de informações de animais

---
+++ controller/controller_Image.php
@@ -0,0 +1,45 @@
+<?php
+
+class Controller_Image{
+
+    private $objectImage;
+
+    public function __construct($id_usuario){
+        $this->objectImage = new Image($id_usuario);
+    }
+
+    public function selectAll(){
+
+        return $this->objectImage->selectAllByUserId();
+    }
+
+    public function deleteImage($id){
+        $this->objectImage->delete($id);
+        $nameFile =  $file["image_source"];
+        unlink($nameFile);
+        
+
+    }
+
+    public function addImage($user_data, $image){
+        extract($user_data);
+
+        $dir = "images/";
+        $file = $image["image"];
+        $image_source = $dir . md5(rand() . date("d-m-Y H:i:s")) . $file["name"];
+
+        $this->objectImage->pushInsert($nome, $image_source, $link);
+
+        if(!is_dir($dir)){
+            mkdir($dir);
+        }
+
+        if(move_uploaded_file($file["tmp_name"],$image_source)){
+            return 1;
+        }else{
+            return 0;
+        };
+
+
+    }
+}
