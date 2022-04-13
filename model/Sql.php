499d1421f5120c7a943130da204268cbf180366f
Davi ferreira de sena <davifsena2@gmail.com>
2022-04-13 08:12:57 -0300

    feat: Método execTransaction criado

--- model/Sql.php
+++ model/Sql.php
@@ -2,7 +2,7 @@
 
 class Sql extends PDO{
 
-    private $conn;
+    public $conn;
 
     public function __construct(){
 
@@ -21,7 +21,7 @@ class Sql extends PDO{
     }
 
     public function execQuery($rawQuery, $params = array()){
-        //echo $rawQuery;
+        echo $rawQuery;
         $stmt = $this->conn->prepare($rawQuery);
         $this->setParams($stmt, $params);
         $stmt->execute();
@@ -35,26 +35,46 @@ class Sql extends PDO{
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+
+    //Metodo para transações
     public function execTransaction($statements){
+
         try {
 
             $this->conn->beginTransaction();
 
                 foreach($statements as $stmt){
-                    var_dump($stmt);
-                    echo "<br>";
+                    /*var_dump($stmt);
+                    echo "<br>";*/
+
+                    $this->execQuery($stmt);
+                    $id = $this->conn->lastInsertId();
+                    var_dump($id);
+
                 }
 
-            $this->conn->commit();
+                $this->conn->commit();
+
+            return (array(
+               "success" => 1 
+            ));
+
         } catch (\Exception $e) {
+
+            $this->conn->rollBack();
+
+            return (array(
+                "success" => 0,
+                "error" => $e->getMessage()
+            ));
         }
         
     }
 }
 
 $teste = new Sql();
-$teste->execTransaction(array(
-    "INSERT INTO tb_animal(nome) VALUES(teste)",
-    "INSERT INTO tb_animal(nome) VALUES(teste)",
-    "INSERT INTO tb_animal(nome) VALUES(teste)"
-));
+var_dump($teste->execTransaction(array(
+    "INSERT INTO tb_animal(nome) VALUES('teste')",
+    "INSERT INTO tb_animal(noe) VALUES('teste1')",
+    "INSERT INTO tb_animal(nome) VALUES('teste2')"
+)));
